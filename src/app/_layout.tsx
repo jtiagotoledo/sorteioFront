97f5dcaa96a6ab25345dcb4d1533956d5ed725a1
jtiagotoledo <jtiagotoledo@gmail.com>
2026-08-23 14:41:27 -0300

    limpeza dir src

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,7 @@
 import "../../global.css";
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
